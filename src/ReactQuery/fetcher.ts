685b1edcbd8642d93a02fa0ce6069649aa3a9c77
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-06-23 22:31:11 -0300

    Add types to fetcher

--- src/ReactQuery/fetcher.ts
+++ src/ReactQuery/fetcher.ts
@@ -1,6 +1,9 @@
 const defaultOptions = { method: "GET" };
 
-export const fetcher = async (url: string, options: RequestInit = defaultOptions): Promise<any> => {
+export const fetcher = async (
+  url: URL | RequestInfo,
+  options: RequestInit = defaultOptions
+): Promise<any> => {
   const resp = await fetch(url, options);
   const data = await resp.json();
   return data;
